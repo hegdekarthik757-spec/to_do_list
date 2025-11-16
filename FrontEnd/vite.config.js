62b7fa2c29dc7e9b50dbf7e47d720cfcf6a64688
Kartikeya <kartikeyahegde@Kartikeyas-MacBook-Pro.local>
2025-11-16 09:17:17 +0700

    Fix GitHub Pages base path

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/to_do_list/',
-  plugins: [react()]
+  base: "/to_do_list/",
 })
+
