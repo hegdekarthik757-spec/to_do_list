9e5242675d878342bc961a971b0c97a6ce308ca4
KARTIKEYA VASANT HEGDE <hegdekarthik757@gmail.com>
2025-11-17 15:59:33 +0700

    Update authServices.js

--- FrontEnd/services/authServices.js
+++ FrontEnd/services/authServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const SERVER_URL = "http://localhost:3000/api";
+const SERVER_URL = "https://todolist-l565.onrender.com/api";
 
 const registerUser = (data) => {
   return axios.post(`${SERVER_URL}/register`, data);
