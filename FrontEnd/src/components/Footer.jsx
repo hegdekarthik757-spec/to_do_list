f5f1c5144b72e037e85b6f7e4a468206a23a382b
KARTIKEYA VASANT HEGDE <hegdekarthik757@gmail.com>
2025-11-18 00:05:24 +0700

    Update Footer.jsx

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -4,8 +4,10 @@ const Footer = () => {
   return (
     <div>
       <footer>
-        <p>If something breaks, blame the code 🧨🐞. 
-          If something works, yes — Mr Miyagi (The Slayer of Bugs) did it. Applaud accordingly 😼✨👏 .</p>
+        <p style = "color:#808080">If something breaks, blame the code 🧨🐞. 
+          <span>
+          If something works, yes — Mr Miyagi (The Slayer of Bugs) did it. Applaud accordingly 😼✨👏 
+          </span>.</p>
       </footer>  
     </div>
   )
